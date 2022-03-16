a50bed6cf4b5c0022d049a20234ca82076bf9735
Carlos Hernandez Topete <chtopete@gmail.com>
2022-03-16 15:41:00 +0900

    React To Do App

--- React-To-Do-List/src/components/App.jsx
+++ React-To-Do-List/src/components/App.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputItem, setInputItem] = useState("");
+  const [items,setItems] = useState([]);
+
+  function handleItem (event) {
+    const newValue = event.target.value;
+    setInputItem(newValue);
+  }
+function addItem()  {
+  setItems((prevValues) => {
+    return [...prevValues, inputItem];
+   })
+   setInputItem("");
+  }
+
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleItem} type="text" value={inputItem} />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map(todoItem => <li>{todoItem}</li>)}
         </ul>
       </div>
     </div>
